7686f2a7d793e8e519480f59415790c73edd766c
Sebastien SALOMON <sebastien.salomon@gmail.com>
2013-09-25 12:09:33 +0200

    Creation du projet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyNotes::Application.config.secret_key_base = 'd3b8ba68c1900e695f9ee07d2fbce6b6715df0edb29e0745292e0827e07fff34d32e6189a06474db372d0d01ea648ee0387fc253f5e87f4cc6230be4ff4febb6'
